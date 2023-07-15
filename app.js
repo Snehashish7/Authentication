6bbfe4015d8dacc2f04dd7f07d4ab0d8187f6ad7
Snehashish7 <snehashishghosh21@gmail.com>
2023-07-15 22:14:48 +0530

    added facebook authentication and styled auth card

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const session = require('express-session')
 const passport = require('passport')
 const passportLocalMongoose = require('passport-local-mongoose')
 const GoogleStrategy = require('passport-google-oauth20').Strategy; //we will use this as a passport strategy.
+const FacebookStrategy = require('passport-facebook').Strategy;
 const findOrCreate = require('mongoose-findorcreate');
 
 const app = express();
@@ -26,13 +27,15 @@ app.use(passport.session())
 mongoose.connect('mongodb://127.0.0.1:27017/userDB', { useNewUrlParser: true });
 // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 const userSchema = new mongoose.Schema({
+  username:String,
   email: String,
   password: String,
   googleId: String,  //will contain the profileId sent by google which will be used to find existing user or create new user.
+  facebookId: String,
   secret: Array
 });
 
-userSchema.plugin(passportLocalMongoose) /*only works if the schema is a mongoose schema and not a simple JS schema.
+userSchema.plugin(passportLocalMongoose, {usernameUnique: false}) /*only works if the schema is a mongoose schema and not a simple JS schema.
   this is what we will use to hash and salt password and store user data in database.
   */
 userSchema.plugin(findOrCreate)
@@ -52,19 +55,30 @@ passport.deserializeUser(function (user, done) {
 
 passport.use(new GoogleStrategy({ //place below serialize and deserialize. We cant
   //place above session since then it cannot save the user session.
-  clientID: process.env.CLIENT_ID,
-  clientSecret: process.env.CLIENT_SECRET,
+  clientID: process.env.GCLIENT_ID,
+  clientSecret: process.env.GCLIENT_SECRET,
   callbackURL: "http://localhost:3000/auth/google/secrets",
 },
   function (accessToken, refreshToken, profile, cb) { //google sends us back accesstoken(user data), profile(email,google ID and more)
     // console.log(profile);
-    User.findOrCreate({ googleId: profile.id }, function (err, user) {  //we have to save the googleId in the database so that that user 
+    User.findOrCreate({ googleId: profile.id, username: profile.id  }, function (err, user) {  //we have to save the googleId in the database so that that user 
       //can be automatically identified when he/she tries to login again
       return cb(err, user);
     })
   }
 ))
 
+passport.use(new FacebookStrategy({
+  clientID: process.env.FCLIENT_ID,
+  clientSecret: process.env.FCLIENT_SECRET,
+  callbackURL: "http://localhost:3000/auth/facebook/secrets"
+},
+function(accessToken, refreshToken, profile, cb) {
+  User.findOrCreate({ facebookId: profile.id, username: profile.id }, function (err, user) {
+    return cb(err, user);
+  });
+}
+));
 
 app.get("/", (req, res) => {
   res.render('home')
@@ -79,6 +93,15 @@ app.get("/auth/google/secrets",
   passport.authenticate("google", { failureRedirect: "/login" }),
   function (req, res) {
     res.redirect("/secrets");
+  });
+
+app.get('/auth/facebook', passport.authenticate('facebook'));
+
+app.get('/auth/facebook/secrets',
+  passport.authenticate('facebook', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/secrets');
   });
 
 app.get("/login", (req, res) => {
