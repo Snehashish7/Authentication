f82237a69c342d59391fb3352c10e299aa275d58
Snehashish7 <snehashishghosh21@gmail.com>
2023-07-16 20:58:28 +0530

    changes

--- app.js
+++ app.js
@@ -236,7 +236,12 @@ app.post("/submit", function (req, res) {
 // });
 
 
-app.listen(3000, () => {
-  console.log("Server is runing on port 3000...   ")
-})
+let port = process.env.PORT;
+if (port == null || port == "") {
+    port = 3000;
+}
+
+app.listen(port, function () {
+    console.log("Server started succesfully");
+});
 
